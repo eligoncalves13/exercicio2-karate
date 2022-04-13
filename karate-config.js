568ee77fb837cfda62e488417b324096c4955eb3
Élida <elidabeatriz13@gmail.com>
2022-04-13 20:59:22 -0300

    segundo commit

---
+++ karate-config.js
@@ -0,0 +1,21 @@
+function setup() {
+    var ambiente = karate.env;
+    var config = {
+        baseUrl: "https://petstore.swagger.io/v2"
+    }
+    
+    if(ambiente == "dev"){
+        karate.configure('logPrettyRequest', true); 
+        karate.configure('logPrettyResponse', true); 
+    }
+
+    karate.configure("headers", {
+        operador: "Élida",
+        framework: "karate"
+    });
+
+    karate.configure('connectTimeout', 40000);
+    karate.configure('readTimeout', 40000); 
+
+    return config;
+}
